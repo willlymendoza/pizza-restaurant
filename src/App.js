a7f359d43c5199f9a6c3dd9d2b472dfeb02882cb
Willian <wyllmh5@gmail.com>
2020-08-13 18:48:20 -0600

    Moved files and reorginized

--- src/App.js
+++ src/App.js
@@ -1,12 +1,24 @@
-import React, { Fragment } from "react";
+import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "components/Header";
+import Main from "components/MainContainer";
+import Home from "pages/Home";
+import Error404 from "components/Error404";
 import "./App.scss";
-import Header from "./pages/Header";
 
 function App() {
   return (
-    <Fragment>
+    <Router>
       <Header />
-    </Fragment>
+
+      <Main>
+        <Switch>
+          <Route path="/" exact component={Home} />
+
+          <Route path="*" component={Error404}></Route>
+        </Switch>
+      </Main>
+    </Router>
   );
 }
 

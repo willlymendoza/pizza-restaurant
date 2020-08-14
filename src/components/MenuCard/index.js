8e628b5e00b3971bc996fe27e1f93522ed100011
Willian <wyllmh5@gmail.com>
2020-08-13 19:12:25 -0600

    Added MenuCard component

---
+++ src/components/MenuCard/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import "./styles.scss";
+import pizza from "assets/img/pizza2.jpg";
+
+const MenuCard = ({ data }) => {
+  return (
+    <div className="menu-card">
+      <img src={pizza} alt="pizza-menu" />
+      <div className="menu-card-content">
+        <h3>{data.title}</h3>
+        <p>{data.description}</p>
+      </div>
+      <span className="menu-card-price">${data.price}</span>
+    </div>
+  );
+};
+
+export default MenuCard;

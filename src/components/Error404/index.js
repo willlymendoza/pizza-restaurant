32a02087199995e1dd67a1b691b1e417261e797a
Willian <wyllmh5@gmail.com>
2020-08-13 18:45:26 -0600

    Added error 404 component

---
+++ src/components/Error404/index.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./styles.scss";
+
+const Error404 = () => {
+  return (
+    <div className="error-404">
+      <h1>404</h1>
+      <h3>NOT FOUND</h3>
+    </div>
+  );
+};
+
+export default Error404;
